de16d74e85be059957839071849dcbe3537224ec
zhiyan114 <zhiyan114@zhiyan114.com>
2023-11-28 19:16:03 -0500

    Some okay navbar?

--- src/app/Component/topbar.tsx
+++ src/app/Component/topbar.tsx
@@ -2,16 +2,21 @@
 
 import React from "react";
 import style from './topbar.module.css';
-
+import Link from "next/link";
 
 export default class NavBar extends React.Component {
     render() {
-        return (<div className={style.container}>
+        return (<div className={style.topbar}>
             <div>
                 <h2>{"{Our Name Here}"}</h2>
             </div>
-            <div>
-                <p>TaB</p>
+            <div className={style.navContainer}>
+                <Link className={style.navbtn} href="/">Home</Link>
+                <Link className={style.navbtn} href="/literacy">Multiple Literacies</Link>
+                <Link className={style.navbtn} href="/context">Variation Context</Link>
+                <Link className={style.navbtn} href="/decision">Decision Making</Link>
+                <Link className={style.navbtn} href="/power">Writing/Power</Link>
+                <Link className={style.navbtn} href="/revision">Revision</Link>
             </div>
         </div>)
     }
