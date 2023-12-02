bc72ee0159e9735f60361e2ab086aef85a7da245
zhiyan114 <zhiyan114@zhiyan114.com>
2023-12-02 01:27:46 -0500

    Finish Decision Page

--- src/app/context/page.tsx
+++ src/app/context/page.tsx
@@ -9,7 +9,9 @@ export default function Home() {
       <Topbar/>
       <Header text="Variation Context" subtext='Students will be able to identify, analyze, and reflect on variation in rhetorical and linguistic patterns, including their own, from a range of contexts (e.g cultural, digital, workplace, and/or academic)'/>
       <div className="cardContainer">
-
+        <ContentCard image='/MA2_context.png'>
+          <p>For the MA2, I was able to demonstrate skill by analyzing deeper into why my target audience may not want to stop using textbook. By doing this, it shows that I'm able to analyze from multiple perspective and reasoning rather than guessing one.</p>
+        </ContentCard>
       </div>
     </Fragment>
   )
--- src/app/decision/page.tsx
+++ src/app/decision/page.tsx
@@ -1,5 +1,6 @@
 import Topbar from '../Component/topbar';
 import Header from '../Component/header';
+import ContentCard from '../Component/contentCard';
 import { Fragment } from 'react';
 
 export default function Home() {
@@ -7,6 +8,15 @@ export default function Home() {
     <Fragment>
       <Topbar/>
       <Header text="Decision Making" subtext='Students will be able to produce writing that demonstrates their ability to navigate choices and constraints when writing for specific audiences, genres, and purposes'/>
+      <div className='cardContainer'>
+        <ContentCard image='MA2_decision.png'>
+          <p>For this MA2, I was able to demonstrate one of the few compromises I've made throughout the development of the paper. For the audience, I had three (which can be seen on the artifact), but after comparing each pros and cons, I was able to ultimately made the correct decision and selected professors.</p>
+        </ContentCard>
+        <ContentCard image='MA3_Decision.png'>
+          <p>For my MA3, the reason for selection PowToon as my animation is not only to try and create an entertaining content for the reader, but also some constraint within myself</p>
+          <p>Some of my weaknesses including public or even recording speak, so I didn't include my voice. Other weaknesses is that I've never made a proper video as an assignment before, so my goal at the time was to pick something that was manageable, which PowToon did.</p>
+        </ContentCard>
+      </div>
     </Fragment>
   )
 }
